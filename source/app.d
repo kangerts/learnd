00db57331994f0988224532c3fdf40849e615202
kangert <kangert@qq.com>
2021-06-18 09:22:02 +0800

    优化

--- source/app.d
+++ source/app.d
@@ -1,6 +1,23 @@
+module app;
+
 import std.stdio;
+import std.conv : to;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+    /** 获取当前文件绝对路径 */
+    string path = to!string(__FILE_FULL_PATH__);
+    string* ptr_string = null;
+    ptr_string = &path;
+    auto aa = getFullPath(ptr_string);
+    writeln(aa);
+}
+
+/** 
+* 获取完整路径
+*/
+string getFullPath(string* path)
+{
+    string a = *path ~ "\t哈哈哈哈哈";
+    return a;
 }
